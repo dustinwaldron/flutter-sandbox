4993cbf7cca8c7f5ec0b79da7d1a29c33a876c0f
dustinwaldron <waldrd11@gmail.com>
2020-09-23 17:59:07 -0400

    make ninja id card stateful

--- ninja_id_app/ninja_id_app/lib/main.dart
+++ ninja_id_app/ninja_id_app/lib/main.dart
@@ -4,7 +4,15 @@ void main() {
   runApp(MaterialApp(home: NinjaCard()));
 }
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +22,15 @@ class NinjaCard extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.grey[850],
           elevation: 0.0,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              ninjaLevel += 1;
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.grey[800],
         ),
         body: Padding(
             padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
@@ -36,7 +53,7 @@ class NinjaCard extends StatelessWidget {
                 Text('CURRENT NINJA LEVEL',
                     style: TextStyle(letterSpacing: 2, color: Colors.grey)),
                 SizedBox(height: 10),
-                Text('8',
+                Text("$ninjaLevel",
                     style: TextStyle(letterSpacing: 2, fontSize: 28, fontWeight: FontWeight.bold, color: Colors.amberAccent[200])),
                 SizedBox(height: 30),
                 Row(
@@ -54,3 +71,4 @@ class NinjaCard extends StatelessWidget {
             )));
   }
 }
+
